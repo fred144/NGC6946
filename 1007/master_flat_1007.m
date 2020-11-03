7009a3fcfc8066e2f2112ae1a4e3db0561eadf95
fred144 <61477599+fred144@users.noreply.github.com>
2020-11-03 18:43:40 -0500

    Add files via upload

---
+++ 1007/master_flat_1007.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = master_flat_jcb_007(masterbias,masterdark,t_flat,t_dark)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
