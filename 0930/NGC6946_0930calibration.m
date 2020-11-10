f093477e136390de95a8607bb4a2d0c6df17396f
fred144 <61477599+fred144@users.noreply.github.com>
2020-11-09 21:31:24 -0500

    Add files via upload

---
+++ 0930/NGC6946_0930calibration.m
@@ -0,0 +1,85 @@
+%% 
+% Note, there are no flats taken for this night, so we will use the flats 
+% taken for night 10/07, make sure those files are added to this folder.
+%the darks and biases are taken on 09/30
+master_bias = master_bias_0930;
+master_dark = master_dark_0930(master_bias); 
+t_flat = 5;
+t_dark = 300;
+t_science = 300;
+master_flat_jcb = master_flat_jcb_1007(master_bias, master_dark ,t_flat ,t_dark);
+normalized_master_flat_jcb = master_flat_jcb ./ mean (master_flat_jcb, 'all'); 
+
+%read in the ngc6946 in JC_B filter
+ngc6946_jcb_raw_01 = rfits('NGC6946-001-jcb.fit');ngc6946_jcb_raw_01 = ngc6946_jcb_raw_01.data;  
+ngc6946_jcb_raw_02 = rfits('NGC6946-002-jcb.fit');ngc6946_jcb_raw_02 = ngc6946_jcb_raw_02.data; 
+ngc6946_jcb_raw_03 = rfits('NGC6946-003-jcb.fit');ngc6946_jcb_raw_03 = ngc6946_jcb_raw_03.data; 
+ngc6946_jcb_raw_04 = rfits('NGC6946-004-jcb.fit');ngc6946_jcb_raw_04 = ngc6946_jcb_raw_04.data;
+ngc6946_jcb_raw_05 = rfits('NGC6946-005-jcb.fit');ngc6946_jcb_raw_05 = ngc6946_jcb_raw_05.data;
+%meridian flipped 
+ngc6946_jcb_raw_06 = rfits('NGC6946b-001-jcb.fit');ngc6946_jcb_raw_06 = flipud(fliplr(ngc6946_jcb_raw_06.data)); 
+ngc6946_jcb_raw_07 = rfits('NGC6946b-002-jcb.fit');ngc6946_jcb_raw_07 = flipud(fliplr(ngc6946_jcb_raw_07.data));
+ngc6946_jcb_raw_08 = rfits('NGC6946b-003-jcb.fit');ngc6946_jcb_raw_08 = flipud(fliplr(ngc6946_jcb_raw_08.data));
+ngc6946_jcb_raw_09 = rfits('NGC6946b-004-jcb.fit');ngc6946_jcb_raw_09 = flipud(fliplr(ngc6946_jcb_raw_09.data));
+ngc6946_jcb_raw_10 = rfits('NGC6946b-005-jcb.fit');ngc6946_jcb_raw_10 = flipud(fliplr(ngc6946_jcb_raw_10.data));
+
+ngc6946_JCB_calibrated_01 = (ngc6946_jcb_raw_01 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_02 = (ngc6946_jcb_raw_02 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_03 = (ngc6946_jcb_raw_03 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_04 = (ngc6946_jcb_raw_04 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_05 = (ngc6946_jcb_raw_05 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_06 = (ngc6946_jcb_raw_06 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_07 = (ngc6946_jcb_raw_07 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_08 = (ngc6946_jcb_raw_08 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_09 = (ngc6946_jcb_raw_09 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+ngc6946_JCB_calibrated_10 = (ngc6946_jcb_raw_10 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_jcb;
+
+
+%%
+%The darks and the biases are the same; only flats change. 
+master_flat_ha = master_flat_ha_1007(master_bias, master_dark ,t_flat ,t_dark);
+normalized_master_flat_ha = master_flat_ha ./ mean (master_flat_ha, 'all'); 
+
+%read in the ngc6946 in Ha filter
+ngc6946_ha_raw_01 = rfits('NGC6946-001-ha.fit');ngc6946_ha_raw_01 = ngc6946_ha_raw_01.data;  
+ngc6946_ha_raw_02 = rfits('NGC6946-002-ha.fit');ngc6946_ha_raw_02 = ngc6946_ha_raw_02.data; 
+ngc6946_ha_raw_03 = rfits('NGC6946-003-ha.fit');ngc6946_ha_raw_03 = ngc6946_ha_raw_03.data; 
+ngc6946_ha_raw_04 = rfits('NGC6946-004-ha.fit');ngc6946_ha_raw_04 = ngc6946_ha_raw_04.data;
+ngc6946_ha_raw_05 = rfits('NGC6946-005-ha.fit');ngc6946_ha_raw_05 = ngc6946_ha_raw_05.data;
+%meridian flipped 
+ngc6946_ha_raw_06 = rfits('NGC6946b-001-ha.fit');ngc6946_ha_raw_06 = flipud(fliplr(ngc6946_ha_raw_06.data)); 
+ngc6946_ha_raw_07 = rfits('NGC6946b-002-ha.fit');ngc6946_ha_raw_07 = flipud(fliplr(ngc6946_ha_raw_07.data));
+ngc6946_ha_raw_08 = rfits('NGC6946b-003-ha.fit');ngc6946_ha_raw_08 = flipud(fliplr(ngc6946_ha_raw_08.data));
+ngc6946_ha_raw_09 = rfits('NGC6946b-004-ha.fit');ngc6946_ha_raw_09 = flipud(fliplr(ngc6946_ha_raw_09.data));
+ngc6946_ha_raw_10 = rfits('NGC6946b-005-ha.fit');ngc6946_ha_raw_10 = flipud(fliplr(ngc6946_ha_raw_10.data));
+
+
+ngc6946_ha_calibrated_01 = (ngc6946_ha_raw_01 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_02 = (ngc6946_ha_raw_02 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_03 = (ngc6946_ha_raw_03 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_04 = (ngc6946_ha_raw_04 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_05 = (ngc6946_ha_raw_05 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_06 = (ngc6946_ha_raw_06 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_07 = (ngc6946_ha_raw_07 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_08 = (ngc6946_ha_raw_08 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_09 = (ngc6946_ha_raw_09 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+ngc6946_ha_calibrated_10 = (ngc6946_ha_raw_10 - (t_science/t_dark)*master_dark - master_bias) ./ normalized_master_flat_ha;
+
+%%
+close all
+figure(1)
+colormap(gray(256));
+imagesc(rot90(ngc6946_ha_raw_10), [1000,1200]); colorbar();
+
+figure(2)
+colormap(gray(256));
+imagesc(rot90(ngc6946_jcb_raw_10), [1000,2500]); colorbar();
+
+
+figure(3)
+colormap(gray(256));
+imagesc(rot90(ngc6946_ha_calibrated_10), [0,500]); colorbar();
+
+figure(4)
+colormap(gray(256));
+imagesc(rot90(ngc6946_JCB_calibrated_10), [500,1700]); colorbar();
---
+++ 0930/master_flat_ha_1007.m
@@ -0,0 +1,44 @@
+function [master_flat_array_ha_1007] = master_flat_ha_1007(masterbias, masterdark ,t_flat ,t_dark)
+%%computes the master flat for ha given the master bias and master dark
+%read in Ha flats
+ngc6946_flat_ha_01 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_01 = ngc6946_flat_ha_01.data;
+ngc6946_flat_ha_02 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_02 = ngc6946_flat_ha_02.data;
+ngc6946_flat_ha_03 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_03 = ngc6946_flat_ha_03.data;
+ngc6946_flat_ha_04 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_04 = ngc6946_flat_ha_04.data;
+ngc6946_flat_ha_05 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_05 = ngc6946_flat_ha_05.data;
+ngc6946_flat_ha_06 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_06 = ngc6946_flat_ha_06.data;
+ngc6946_flat_ha_07 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_07 = ngc6946_flat_ha_07.data;
+ngc6946_flat_ha_08 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_08 = ngc6946_flat_ha_08.data;
+ngc6946_flat_ha_09 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_09 = ngc6946_flat_ha_09.data;
+ngc6946_flat_ha_10 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_10 = ngc6946_flat_ha_10.data;
+ngc6946_flat_ha_11 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_11 = ngc6946_flat_ha_11.data;
+ngc6946_flat_ha_12 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_12 = ngc6946_flat_ha_12.data;
+ngc6946_flat_ha_13 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_13 = ngc6946_flat_ha_13.data;
+ngc6946_flat_ha_14 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_14 = ngc6946_flat_ha_14.data;
+ngc6946_flat_ha_15 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_15 = ngc6946_flat_ha_15.data;
+ngc6946_flat_ha_16 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_16 = ngc6946_flat_ha_16.data;
+ngc6946_flat_ha_17 = rfits('flats-001-ha.fit'); ngc6946_flat_ha_17 = ngc6946_flat_ha_17.data;
+
+
+flat_calib_01 = ngc6946_flat_ha_01 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_02 = ngc6946_flat_ha_02 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_03 = ngc6946_flat_ha_03 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_04 = ngc6946_flat_ha_04 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_05 = ngc6946_flat_ha_05 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_06 = ngc6946_flat_ha_06 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_07 = ngc6946_flat_ha_07 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_08 = ngc6946_flat_ha_08 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_09 = ngc6946_flat_ha_09 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_10 = ngc6946_flat_ha_10 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_11 = ngc6946_flat_ha_11 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_12 = ngc6946_flat_ha_12 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_13 = ngc6946_flat_ha_13 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_14 = ngc6946_flat_ha_14 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_15 = ngc6946_flat_ha_15 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_16 = ngc6946_flat_ha_16 - masterbias - (t_flat/t_dark)*masterdark;
+flat_calib_17 = ngc6946_flat_ha_17 - masterbias - (t_flat/t_dark)*masterdark;
+
+flat_stacked_ha_1007 = cat(3, flat_calib_01, flat_calib_02, flat_calib_03, flat_calib_04, flat_calib_05, flat_calib_06, flat_calib_07, flat_calib_08, flat_calib_09, flat_calib_10, flat_calib_11, flat_calib_12, flat_calib_13, flat_calib_14, flat_calib_15, flat_calib_16, flat_calib_17); 
+master_flat_array_ha_1007 = median (flat_stacked_ha_1007, 3);
+end
+
