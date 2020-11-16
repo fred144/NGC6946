032dcdb37a9e2113ddd209eac671a34c722e1693
fred144 <61477599+fred144@users.noreply.github.com>
2020-11-16 18:39:38 -0500

    Add files via upload

---
+++ NGC6946_Coadding.m
@@ -0,0 +1,149 @@
+% Don't forget to add all the calibrated .fits files into the file path.
+% ctrl + enter on a section runs only that section
+%% Opening All 10/08 Calibrated Frames
+HA1008_1 = rfits('1008-calibrated-ha-1.fit'); HA1008_1 = HA1008_1.data; 
+HA1008_2 = rfits('1008-calibrated-ha-2.fit'); HA1008_2 = HA1008_2.data;
+HA1008_3 = rfits('1008-calibrated-ha-3.fit'); HA1008_3 = HA1008_3.data;
+HA1008_4 = rfits('1008-calibrated-ha-4.fit'); HA1008_4 = HA1008_4.data;
+HA1008_5 = rfits('1008-calibrated-ha-5.fit'); HA1008_5 = HA1008_5.data;
+HA1008_6 = rfits('1008-calibrated-ha-6.fit'); HA1008_6 = HA1008_6.data;
+HA1008_7 = rfits('1008-calibrated-ha-7.fit'); HA1008_7 = HA1008_7.data;
+HA1008_8 = rfits('1008-calibrated-ha-8.fit'); HA1008_8 = HA1008_8.data;
+HA1008_9 = rfits('1008-calibrated-ha-9.fit'); HA1008_9 = HA1008_9.data;
+HA1008_10 = rfits('1008-calibrated-ha-10.fit'); HA1008_10 = HA1008_10.data;
+HA1008_11 = rfits('1008-calibrated-ha-11.fit'); HA1008_11 = HA1008_11.data;
+%---------------------------------------------------------------------------
+JCB1008_1 = rfits('1008-calibrated-jcb-1.fit'); JCB1008_1 = JCB1008_1.data; 
+JCB1008_2 = rfits('1008-calibrated-jcb-2.fit'); JCB1008_2 = JCB1008_2.data;
+JCB1008_3 = rfits('1008-calibrated-jcb-3.fit'); JCB1008_3 = JCB1008_3.data;
+JCB1008_4 = rfits('1008-calibrated-jcb-4.fit'); JCB1008_4 = JCB1008_4.data;
+JCB1008_5 = rfits('1008-calibrated-jcb-5.fit'); JCB1008_5 = JCB1008_5.data;
+JCB1008_6 = rfits('1008-calibrated-jcb-6.fit'); JCB1008_6 = JCB1008_6.data;
+JCB1008_7 = rfits('1008-calibrated-jcb-7.fit'); JCB1008_7 = JCB1008_7.data;
+JCB1008_8 = rfits('1008-calibrated-jcb-8.fit'); JCB1008_8 = JCB1008_8.data;
+JCB1008_9 = rfits('1008-calibrated-jcb-9.fit'); JCB1008_9 = JCB1008_9.data;
+JCB1008_10 = rfits('1008-calibrated-jcb-10.fit'); JCB1008_10 = JCB1008_10.data;
+JCB1008_11 = rfits('1008-calibrated-jcb-11.fit'); JCB1008_11 = JCB1008_11.data;
+%% Opening All 10/07 Calibrated Frames
+HA1007_1 = rfits('1007-calibrated-ha-1.fit'); HA1007_1 = HA1007_1.data; 
+HA1007_2 = rfits('1007-calibrated-ha-2.fit'); HA1007_2 = HA1007_2.data;
+HA1007_3 = rfits('1007-calibrated-ha-3.fit'); HA1007_3 = HA1007_3.data;
+HA1007_4 = rfits('1007-calibrated-ha-4.fit'); HA1007_4 = HA1007_4.data;
+HA1007_5 = rfits('1007-calibrated-ha-5.fit'); HA1007_5 = HA1007_5.data;
+HA1007_6 = rfits('1007-calibrated-ha-6.fit'); HA1007_6 = HA1007_6.data;
+HA1007_7 = rfits('1007-calibrated-ha-7.fit'); HA1007_7 = HA1007_7.data;
+HA1007_8 = rfits('1007-calibrated-ha-8.fit'); HA1007_8 = HA1007_8.data;
+HA1007_9 = rfits('1007-calibrated-ha-9.fit'); HA1007_9 = HA1007_9.data;
+HA1007_10 = rfits('1007-calibrated-ha-10.fit'); HA1007_10 = HA1007_10.data;
+HA1007_11 = rfits('1007-calibrated-ha-11.fit'); HA1007_11 = HA1007_11.data;
+HA1007_12 = rfits('1007-calibrated-ha-12.fit'); HA1007_12 = HA1007_12.data;
+HA1007_13 = rfits('1007-calibrated-ha-13.fit'); HA1007_13 = HA1007_13.data;
+HA1007_14 = rfits('1007-calibrated-ha-14.fit'); HA1007_14 = HA1007_14.data;
+HA1007_15 = rfits('1007-calibrated-ha-15.fit'); HA1007_15 = HA1007_15.data;
+HA1007_16 = rfits('1007-calibrated-ha-16.fit'); HA1007_16 = HA1007_16.data;
+HA1007_17 = rfits('1007-calibrated-ha-17.fit'); HA1007_17 = HA1007_17.data;
+HA1007_18 = rfits('1007-calibrated-ha-18.fit'); HA1007_18 = HA1007_18.data;
+HA1007_19 = rfits('1007-calibrated-ha-19.fit'); HA1007_19 = HA1007_19.data;
+HA1007_20 = rfits('1007-calibrated-ha-20.fit'); HA1007_20 = HA1007_20.data;
+HA1007_21 = rfits('1007-calibrated-ha-21.fit'); HA1007_21 = HA1007_21.data;
+HA1007_22 = rfits('1007-calibrated-ha-22.fit'); HA1007_22 = HA1007_22.data;
+HA1007_23 = rfits('1007-calibrated-ha-23.fit'); HA1007_23 = HA1007_23.data;
+HA1007_24 = rfits('1007-calibrated-ha-24.fit'); HA1007_24 = HA1007_24.data;
+HA1007_25 = rfits('1007-calibrated-ha-25.fit'); HA1007_25 = HA1007_25.data;
+HA1007_26 = rfits('1007-calibrated-ha-26.fit'); HA1007_26 = HA1007_26.data;
+HA1007_27 = rfits('1007-calibrated-ha-27.fit'); HA1007_27 = HA1007_27.data;
+HA1007_28 = rfits('1007-calibrated-ha-28.fit'); HA1007_28 = HA1007_28.data;
+HA1007_29 = rfits('1007-calibrated-ha-29.fit'); HA1007_29 = HA1007_29.data;
+HA1007_30 = rfits('1007-calibrated-ha-30.fit'); HA1007_30 = HA1007_30.data;
+HA1007_31 = rfits('1007-calibrated-ha-31.fit'); HA1007_31 = HA1007_31.data;
+HA1007_32 = rfits('1007-calibrated-ha-32.fit'); HA1007_32 = HA1007_32.data;
+HA1007_33 = rfits('1007-calibrated-ha-33.fit'); HA1007_33 = HA1007_33.data;
+HA1007_34 = rfits('1007-calibrated-ha-34.fit'); HA1007_34 = HA1007_34.data;
+HA1007_35 = rfits('1007-calibrated-ha-35.fit'); HA1007_35 = HA1007_35.data;
+HA1007_36 = rfits('1007-calibrated-ha-36.fit'); HA1007_36 = HA1007_36.data;
+HA1007_37 = rfits('1007-calibrated-ha-37.fit'); HA1007_37 = HA1007_37.data;
+HA1007_38 = rfits('1007-calibrated-ha-38.fit'); HA1007_38 = HA1007_38.data;
+HA1007_39 = rfits('1007-calibrated-ha-39.fit'); HA1007_39 = HA1007_39.data;
+HA1007_40 = rfits('1007-calibrated-ha-40.fit'); HA1007_40 = HA1007_40.data;
+%---------------------------------------------------------------------------------
+JCB1007_1 = rfits('1007-calibrated-jcb-1.fit'); JCB1007_1 = JCB1007_1.data; 
+JCB1007_2 = rfits('1007-calibrated-jcb-2.fit'); JCB1007_2 = JCB1007_2.data;
+JCB1007_3 = rfits('1007-calibrated-jcb-3.fit'); JCB1007_3 = JCB1007_3.data;
+JCB1007_4 = rfits('1007-calibrated-jcb-4.fit'); JCB1007_4 = JCB1007_4.data;
+JCB1007_5 = rfits('1007-calibrated-jcb-5.fit'); JCB1007_5 = JCB1007_5.data;
+JCB1007_6 = rfits('1007-calibrated-jcb-6.fit'); JCB1007_6 = JCB1007_6.data;
+JCB1007_7 = rfits('1007-calibrated-jcb-7.fit'); JCB1007_7 = JCB1007_7.data;
+JCB1007_8 = rfits('1007-calibrated-jcb-8.fit'); JCB1007_8 = JCB1007_8.data;
+JCB1007_9 = rfits('1007-calibrated-jcb-9.fit'); JCB1007_9 = JCB1007_9.data;
+JCB1007_10 = rfits('1007-calibrated-jcb-10.fit'); JCB1007_10 = JCB1007_10.data;
+JCB1007_11 = rfits('1007-calibrated-jcb-11.fit'); JCB1007_11 = JCB1007_11.data;
+JCB1007_12 = rfits('1007-calibrated-jcb-12.fit'); JCB1007_12 = JCB1007_12.data;
+JCB1007_13 = rfits('1007-calibrated-jcb-13.fit'); JCB1007_13 = JCB1007_13.data;
+JCB1007_14 = rfits('1007-calibrated-jcb-14.fit'); JCB1007_14 = JCB1007_14.data;
+JCB1007_15 = rfits('1007-calibrated-jcb-15.fit'); JCB1007_15 = JCB1007_15.data;
+JCB1007_16 = rfits('1007-calibrated-jcb-16.fit'); JCB1007_16 = JCB1007_16.data;
+JCB1007_17 = rfits('1007-calibrated-jcb-17.fit'); JCB1007_17 = JCB1007_17.data;
+JCB1007_18 = rfits('1007-calibrated-jcb-18.fit'); JCB1007_18 = JCB1007_18.data;
+JCB1007_19 = rfits('1007-calibrated-jcb-19.fit'); JCB1007_19 = JCB1007_19.data;
+JCB1007_20 = rfits('1007-calibrated-jcb-20.fit'); JCB1007_20 = JCB1007_20.data;
+JCB1007_21 = rfits('1007-calibrated-jcb-21.fit'); JCB1007_21 = JCB1007_21.data;
+JCB1007_22 = rfits('1007-calibrated-jcb-22.fit'); JCB1007_22 = JCB1007_22.data;
+JCB1007_23 = rfits('1007-calibrated-jcb-23.fit'); JCB1007_23 = JCB1007_23.data;
+JCB1007_24 = rfits('1007-calibrated-jcb-24.fit'); JCB1007_24 = JCB1007_24.data;
+JCB1007_25 = rfits('1007-calibrated-jcb-25.fit'); JCB1007_25 = JCB1007_25.data;
+JCB1007_26 = rfits('1007-calibrated-jcb-26.fit'); JCB1007_26 = JCB1007_26.data;
+JCB1007_27 = rfits('1007-calibrated-jcb-27.fit'); JCB1007_27 = JCB1007_27.data;
+JCB1007_28 = rfits('1007-calibrated-jcb-28.fit'); JCB1007_28 = JCB1007_28.data;
+JCB1007_29 = rfits('1007-calibrated-jcb-29.fit'); JCB1007_29 = JCB1007_29.data;
+JCB1007_30 = rfits('1007-calibrated-jcb-30.fit'); JCB1007_30 = JCB1007_30.data;
+JCB1007_31 = rfits('1007-calibrated-jcb-31.fit'); JCB1007_31 = JCB1007_31.data;
+JCB1007_32 = rfits('1007-calibrated-jcb-32.fit'); JCB1007_32 = JCB1007_32.data;
+JCB1007_33 = rfits('1007-calibrated-jcb-33.fit'); JCB1007_33 = JCB1007_33.data;
+JCB1007_34 = rfits('1007-calibrated-jcb-34.fit'); JCB1007_34 = JCB1007_34.data;
+JCB1007_35 = rfits('1007-calibrated-jcb-35.fit'); JCB1007_35 = JCB1007_35.data;
+JCB1007_36 = rfits('1007-calibrated-jcb-36.fit'); JCB1007_36 = JCB1007_36.data;
+JCB1007_37 = rfits('1007-calibrated-jcb-37.fit'); JCB1007_37 = JCB1007_37.data;
+JCB1007_38 = rfits('1007-calibrated-jcb-38.fit'); JCB1007_38 = JCB1007_38.data;
+JCB1007_39 = rfits('1007-calibrated-jcb-39.fit'); JCB1007_39 = JCB1007_39.data;
+JCB1007_40 = rfits('1007-calibrated-jcb-40.fit'); JCB1007_40 = JCB1007_40.data;
+%% Opening All 09/30 Calibrated Frames
+HA0930_1 = rfits('0930-calibrated-ha-1.fit'); HA0930_1 = HA0930_1.data; 
+HA0930_2 = rfits('0930-calibrated-ha-2.fit'); HA0930_2 = HA0930_2.data;
+HA0930_3 = rfits('0930-calibrated-ha-3.fit'); HA0930_3 = HA0930_3.data;
+HA0930_4 = rfits('0930-calibrated-ha-4.fit'); HA0930_4 = HA0930_4.data;
+HA0930_5 = rfits('0930-calibrated-ha-5.fit'); HA0930_5 = HA0930_5.data;
+HA0930_6 = rfits('0930-calibrated-ha-6.fit'); HA0930_6 = HA0930_6.data;
+HA0930_7 = rfits('0930-calibrated-ha-7.fit'); HA0930_7 = HA0930_7.data;
+HA0930_8 = rfits('0930-calibrated-ha-8.fit'); HA0930_8 = HA0930_8.data;
+HA0930_9 = rfits('0930-calibrated-ha-9.fit'); HA0930_9 = HA0930_9.data;
+HA0930_10 = rfits('0930-calibrated-ha-10.fit'); HA0930_10 = HA0930_10.data;
+%-----------------------------------------------------------------------------------
+JCB0930_1 = rfits('0930-calibrated-jcb-1.fit'); JCB0930_1 = JCB0930_1.data; 
+JCB0930_2 = rfits('0930-calibrated-jcb-2.fit'); JCB0930_2 = JCB0930_2.data;
+JCB0930_3 = rfits('0930-calibrated-jcb-3.fit'); JCB0930_3 = JCB0930_3.data;
+JCB0930_4 = rfits('0930-calibrated-jcb-4.fit'); JCB0930_4 = JCB0930_4.data;
+JCB0930_5 = rfits('0930-calibrated-jcb-5.fit'); JCB0930_5 = JCB0930_5.data;
+JCB0930_6 = rfits('0930-calibrated-jcb-6.fit'); JCB0930_6 = JCB0930_6.data;
+JCB0930_7 = rfits('0930-calibrated-jcb-7.fit'); JCB0930_7 = JCB0930_7.data;
+JCB0930_8 = rfits('0930-calibrated-jcb-8.fit'); JCB0930_8 = JCB0930_8.data;
+JCB0930_9 = rfits('0930-calibrated-jcb-9.fit'); JCB0930_9 = JCB0930_9.data;
+JCB0930_10 = rfits('0930-calibrated-jcb-10.fit'); JCB0930_10 = JCB0930_10.data;
+%% Coadding
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
